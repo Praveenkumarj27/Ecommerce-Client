37a4a1d3090a86ce2b47a3872d6fce0279b5d259
praveenkumarj27 <jpraveenkumar263@gmail.com>
2022-12-01 14:32:13 +0530

    init

--- src/components/screens/Auth/Login.js
+++ src/components/screens/Auth/Login.js
@@ -21,54 +21,52 @@ const Login = () => {
 
   return (
     <>
-      <h3 className="text-center mt-4">LOGIN NOTEBOOK STORE</h3>
+      <div className="container col-sm-12 col-md-12 col-lg-4 p-0 form">
+        <div className="heading">
+          <h1 className="login">Login</h1>
+        </div>
 
-      <div className="col-lg-8 offset-lg-2">
-        <form className="shadow-lg rounded p-4 mt-4" onSubmit={handleSubmit}>
-          <div className="form-floating mb-3 mt-3">
-            <input
-              type="email"
-              className="form-control"
-              id="email"
-              placeholder="Enter Your Email"
-              name="email"
-              required
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <label htmlFor="email">Enter Your Email</label>
-          </div>
-          <div className="form-floating mb-3">
-            <input
-              type="password"
-              className="form-control"
-              id="password"
-              placeholder="Enter Your Password"
-              name="password"
-              required
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <label htmlFor="password" className="form-label">
-              Enter Your Password
-            </label>
-          </div>
-          <div className="d-grid">
-            <button
-              type="submit"
-              className={`btn ${loading ? "btn-secondary" : "btn-success"}`}
-              disabled={loading}
-            >
-              {loading ? "LOADING . . ." : "LOGIN"}
-            </button>
-          </div>
-          <div className="d-flex align-items-center justify-content-center mt-4">
-            <p className="mb-0 me-2 text-muted">Don't have an account?</p>
-            <Link to="/register" className="btn btn-outline-primary btn-sm">
-              SIGN UP
-            </Link>
-          </div>
-        </form>
+        <div className="container form-body pb-4 ps-4 pe-4">
+          <form onSubmit={handleSubmit}>
+            <div class="mb-1 mt-3">
+              <input
+                type="email"
+                className="form-control"
+                placeholder="Email"
+                id="email"
+                name="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+
+            <div class="mb-3">
+              <input
+                type="password"
+                class="form-control"
+                placeholder="Password"
+                id="password"
+                name="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+
+            <div className="d-grid">
+              <button
+                type="submit"
+                className={`btn ${loading ? "btn-secondary" : "btn-success"}`}
+                disabled={loading}
+              >
+                {loading ? "LOADING . . ." : "LOGIN"}
+              </button>
+            </div>
+            <div className="d-flex align-items-center justify-content-center mt-4">
+              <p className="mb-0 me-2 text-muted">Don't have an account?</p>
+              <Link to="/register">SIGN UP</Link>
+            </div>
+          </form>
+        </div>
       </div>
     </>
   );
--- src/components/screens/Auth/Register.js
+++ src/components/screens/Auth/Register.js
@@ -44,87 +44,75 @@ const Register = () => {
   };
   return (
     <>
-      <h3 className="text-center mt-3">CREATE ACCOUNT</h3>
+      <div className="container col-sm-12 col-md-12 col-lg-4 p-0 form">
+        <div className="heading">
+        <h1 className='signup'>Sign Up</h1>
+        </div>
 
-      <div className="col-lg-8 offset-lg-2">
-        <form className="shadow-lg rounded p-4 mt-4" onSubmit={handleSubmit}>
-          <div className="form-floating mb-3 mt-3">
-            <input
-              type="text"
-              className="form-control"
-              id="name"
-              placeholder="Enter Your Name"
-              name="name"
-              // required
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
-            <label htmlFor="name">Enter Your Name</label>
-          </div>
-          <div className="form-floating mb-3">
-            <input
-              type="email"
-              className="form-control"
-              id="email"
-              placeholder="Enter Your Email"
-              name="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              // required
-            />
-            <label htmlFor="email" className="form-label">
-              Enter Your Email
-            </label>
-          </div>
-          <div className="form-floating mb-3 mt-3">
-            <input
-              type="password"
-              className="form-control"
-              id="password"
-              placeholder="Enter Your Password"
-              name="password"
-              // required
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <label htmlFor="password" className="form-label">
-              Enter Your Password
-            </label>
-          </div>
-          <div className="form-floating mb-3">
-            <input
-              type="password"
-              className="form-control"
-              id="confirm"
-              placeholder="Enter Your Password Again"
-              name="confirm"
-              // required
-              value={confirm}
-              onChange={(e) => setConfirm(e.target.value)}
-            />
-            <label htmlFor="confirm" className="form-label">
-              Enter Your Password Again
-            </label>
-          </div>
-          <div className="d-grid">
-            <button
-              type="submit"
-              className={`btn ${loading ? "btn-secondary" : "btn-success"}`}
-              disabled={loading}
-            >
-              {loading ? "LOADING . . ." : "SIGN IN"}
-            </button>
-            <div className="d-flex justify-content-center pt-4">
-              <GoogleButton onClick={signInWithGoogle} />
+        <div className="container form-body pb-4 ps-4 pe-4">
+          <form onSubmit={handleSubmit}>
+            <div class="mb-3 mt-3">
+              <input
+                type="text"
+                className="form-control"
+                id="name"
+                placeholder="Enter Your Name"
+                name="name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
             </div>
-          </div>
-          <div className="d-flex align-items-center justify-content-center mt-4">
-            <p className="mb-0 me-2 text-muted">I Have Account Login</p>
-            <Link to="/login" className="btn btn-outline-primary btn-sm">
-              LOGIN
-            </Link>
-          </div>
-        </form>
+            <div class="mb-3 mt-3">
+              <input
+                type="email"
+                className="form-control"
+                id="email"
+                placeholder="Enter Your Email"
+                name="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div class="mb-3 mt-3">
+              <input
+                type="password"
+                className="form-control"
+                id="password"
+                placeholder="Enter Your Password"
+                name="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+
+            <div class="mb-3">
+              <input
+                type="password"
+                className="form-control"
+                id="confirm"
+                placeholder="Enter Your Password Again"
+                name="confirm"
+                value={confirm}
+                onChange={(e) => setConfirm(e.target.value)}
+              />
+            </div>
+
+            <div className="d-grid">
+              <button
+                type="submit"
+                className={`btn ${loading ? "btn-secondary" : "btn-success"}`}
+                disabled={loading}
+              >
+                {loading ? "LOADING . . ." : "SIGN IN"}
+              </button>
+            </div>
+
+            <div className="d-flex align-items-center justify-content-center mt-4">
+              <p className="mb-0 me-2 text-muted">I Have Account Login</p>
+              <Link to="/login">LOGIN</Link>
+            </div>
+          </form>
+        </div>
       </div>
     </>
   );
